d96b841e7194c2ad52019f56e646fd03751cabf4
Vince W <vincewallgren@me.com>
2020-10-27 02:28:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  if int & 5 == 0
-    "Buzz"
-  if int & 3 == 0 && int & 5 == 0
-    "FizzBuzz"
+  if int % 5 == 0
+    nil
+  if int % 3 == 0 && int % 5 == 0
+    ArgumentError
   end
 end
